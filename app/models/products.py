61841a8d8044500a72a092f37478757a76e32d03
Pedro França <pedrowill.developer@gmail.com>
2026-02-16 13:42:40 -0300

    update orders products, add new field [stock_status_id]

--- app/models/products.py
+++ app/models/products.py
@@ -27,6 +27,7 @@ class Product(Base):
     origin = Column(String(255))
     payment_date = Column(TIMESTAMP, nullable=True)
     billing_until = Column(TIMESTAMP, nullable=True)
+    status_status_id = Column(Integer, default=0)
 
     # Relationships
     order = relationship(
